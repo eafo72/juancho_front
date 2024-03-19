3280b1908ff62bb694745c881c2f46c85abaebd0
alex <eafo72@gmail.com>
2024-03-19 17:44:46 -0600

    19032024

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const clienteAxios = axios.create({
- //baseURL: "https://juancho-back.onrender.com"
- baseURL: "http://localhost:4000"
+ baseURL: "https://juancho-back.onrender.com"
+ //baseURL: "http://localhost:4000"
 })
 
 export default clienteAxios
